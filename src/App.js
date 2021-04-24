feb99f083c2281d6dd35bdd3f02a6e509ee2553b
bhawna chopra <55947219+bhawnachopra2002@users.noreply.github.com>
2021-04-24 16:38:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bhawna Chopra
+          Learn React
         </a>
       </header>
     </div>
